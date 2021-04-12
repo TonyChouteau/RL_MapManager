27f385ece2d9b159a70cd5fbeb668b0c2cb46812
TonyChouteau <tony.chouteau@outlook.fr>
2021-04-13 01:32:33 +0200

    feat/tests : add tests for read/write and unzip files

---
+++ tests/test-zip.js
@@ -0,0 +1,21 @@
+let fs = require('fs');
+let Zip = require('adm-zip');
+
+const path = './tmp/';
+
+fs.readdir(path, function (err, files) {
+	if (err) {
+		return console.log('Unable to scan directory: ' + err);
+	}
+	files.filter(file => file.includes("zip")).forEach(function (file) {
+		console.log(file);
+		let name = file.replace(".zip$", "").replace(".rar$", "");
+		let zip = new Zip(path + file);
+		zip.getEntries().forEach(function(entry) {
+			let fileName = entry.entryName ;
+			if (fileName .includes(".upk") || fileName.includes(".udk")) {
+				zip.extractEntryTo(fileName, path, false, true);
+			}
+		});
+	});
+});
